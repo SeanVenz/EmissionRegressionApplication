6ad3704265c9d843e8551c6263ba6ecc05393f5f
Andre Enriquez <andre.enriquez@cit.edu>
2023-02-17 00:36:00 +0800

    Update graph

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -47,14 +47,14 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(734, 136);
+            this.textBox1.Location = new System.Drawing.Point(896, 137);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(853, 133);
+            this.button1.Location = new System.Drawing.Point(1015, 134);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 1;
@@ -65,7 +65,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(628, 139);
+            this.label1.Location = new System.Drawing.Point(790, 140);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(88, 16);
             this.label1.TabIndex = 2;
@@ -74,7 +74,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(307, 28);
+            this.label2.Location = new System.Drawing.Point(412, 27);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(341, 16);
             this.label2.TabIndex = 4;
@@ -83,7 +83,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(272, 49);
+            this.label3.Location = new System.Drawing.Point(377, 48);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(409, 16);
             this.label3.TabIndex = 5;
@@ -99,17 +99,21 @@
             this.chart1.Name = "chart1";
             series2.ChartArea = "ChartArea1";
             series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Color = System.Drawing.Color.Maroon;
+            series2.LabelBackColor = System.Drawing.Color.White;
+            series2.LabelBorderColor = System.Drawing.Color.Transparent;
             series2.Legend = "Legend1";
+            series2.LegendText = "X: CO2 Emissions (in kT) Y: Mean Annual Temperature";
             series2.Name = "Series1";
             this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(572, 394);
+            this.chart1.Size = new System.Drawing.Size(734, 422);
             this.chart1.TabIndex = 6;
             this.chart1.Text = "chart1";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(628, 265);
+            this.label4.Location = new System.Drawing.Point(790, 266);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(288, 16);
             this.label4.TabIndex = 7;
@@ -118,7 +122,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(661, 170);
+            this.label5.Location = new System.Drawing.Point(823, 171);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(55, 16);
             this.label5.TabIndex = 8;
@@ -127,7 +131,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(731, 170);
+            this.label6.Location = new System.Drawing.Point(893, 171);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(0, 16);
             this.label6.TabIndex = 9;
@@ -135,7 +139,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(628, 290);
+            this.label7.Location = new System.Drawing.Point(790, 291);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(155, 16);
             this.label7.TabIndex = 10;
@@ -144,7 +148,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(631, 320);
+            this.label8.Location = new System.Drawing.Point(793, 321);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(174, 16);
             this.label8.TabIndex = 11;
@@ -154,7 +158,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(995, 531);
+            this.ClientSize = new System.Drawing.Size(1152, 554);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -168,6 +172,7 @@
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,71 @@ namespace EmissionsRegressionApplication
 {
     public partial class Form1 : Form
     {
+        List<double> xValues = new List<double>();
+        List<double> yValues = new List<double>();
         public Form1()
         {
             InitializeComponent();
+            xValues.Add(72430);
+            yValues.Add(25.66);
+
+            xValues.Add(73190);
+            yValues.Add(25.79);
+
+            xValues.Add(71670);
+            yValues.Add(25.85);
+
+            xValues.Add(72940);
+            yValues.Add(25.73);
+
+            xValues.Add(74770);
+            yValues.Add(25.61);
+
+            xValues.Add(76320);
+            yValues.Add(25.70);
+
+            xValues.Add(78100);
+            yValues.Add(25.80);
+
+            xValues.Add(70810);
+            yValues.Add(25.86);
+
+            xValues.Add(74560);
+            yValues.Add(25.85);
+
+            xValues.Add(76730);
+            yValues.Add(25.52);
+
+            xValues.Add(77520);
+            yValues.Add(25.56);
+
+            xValues.Add(83570);
+            yValues.Add(25.83);
+
+            xValues.Add(84470);
+            yValues.Add(25.63);
+
+            xValues.Add(88060);
+            yValues.Add(25.93);
+
+            xValues.Add(97800);
+            yValues.Add(25.94);
+
+            xValues.Add(104410);
+            yValues.Add(25.78);
+
+            xValues.Add(113670);
+            yValues.Add(26.02);
+
+            xValues.Add(124920);
+            yValues.Add(26.36);
+
+            xValues.Add(136590);
+            yValues.Add(25.91);
+
+            xValues.Add(142240);
+            yValues.Add(26.11);
+
         }
 
         Regression regression = new Regression();
@@ -33,6 +95,11 @@ namespace EmissionsRegressionApplication
 
             label6.Text = result.ToString();
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series["Series1"].Points.DataBindXY(xValues, yValues);
         }
     }
 }
